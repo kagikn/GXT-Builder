88e7249dd73be6aab72a4432f1f30aa1980cf0e2
kagikn <kagikn.3939@gmail.com>
2018-01-27 23:49:33 +0900

    Use Win32 API in Utf8ToUtf16

--- GXT Text Replacer/utility.cpp
+++ GXT Text Replacer/utility.cpp
@@ -12,6 +12,9 @@
 #include <unordered_map>
 
 #include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+
 
 bool Directory::Exists(const std::wstring& dirName_in)
 {
@@ -39,74 +42,13 @@ std::wstring Encoding::AnsiStringToWString(std::string const& src)
 
 std::wstring Encoding::Utf8ToUtf16(const std::string& utf8)
 {
-    std::vector<unsigned long> unicode;
-    size_t i = 0;
-    while (i < utf8.size())
-    {
-        unsigned long uni;
-        size_t todo;
-        bool error = false;
-        unsigned char ch = utf8[i++];
-        if (ch <= 0x7F)
-        {
-            uni = ch;
-            todo = 0;
-        }
-        else if (ch <= 0xBF)
-        {
-            throw std::logic_error("not a UTF-8 string");
-        }
-        else if (ch <= 0xDF)
-        {
-            uni = ch & 0x1F;
-            todo = 1;
-        }
-        else if (ch <= 0xEF)
-        {
-            uni = ch & 0x0F;
-            todo = 2;
-        }
-        else if (ch <= 0xF7)
-        {
-            uni = ch & 0x07;
-            todo = 3;
-        }
-        else
-        {
-            throw std::logic_error("not a UTF-8 string");
-        }
-        for (size_t j = 0; j < todo; ++j)
-        {
-            if (i == utf8.size())
-                throw std::logic_error("not a UTF-8 string");
-            unsigned char ch = utf8[i++];
-            if (ch < 0x80 || ch > 0xBF)
-                throw std::logic_error("not a UTF-8 string");
-            uni <<= 6;
-            uni += ch & 0x3F;
-        }
-        if (uni >= 0xD800 && uni <= 0xDFFF)
-            throw std::logic_error("not a UTF-8 string");
-        if (uni > 0x10FFFF)
-            throw std::logic_error("not a UTF-8 string");
-        unicode.push_back(uni);
-    }
-    std::wstring utf16;
-    for (size_t i = 0; i < unicode.size(); ++i)
-    {
-        unsigned long uni = unicode[i];
-        if (uni <= 0xFFFF)
-        {
-            utf16 += (wchar_t)uni;
-        }
-        else
-        {
-            uni -= 0x10000;
-            utf16 += (wchar_t)((uni >> 10) + 0xD800);
-            utf16 += (wchar_t)((uni & 0x3FF) + 0xDC00);
-        }
-    }
-    return utf16;
+    int iBufferSize = MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, (wchar_t*)NULL, 0);
+
+    std::vector<wchar_t> dest(iBufferSize, L'\0');
+
+    MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), -1, dest.data(), iBufferSize);
+
+    return std::wstring(dest.data(), dest.data() + iBufferSize - 1);
 }
 
 void EntryLoader::LoadFileContent(const wchar_t* fileName, std::unordered_map<std::string, std::string>& entryMap, std::ofstream& logFile)
@@ -142,6 +84,7 @@ void EntryLoader::LoadFileContent(const wchar_t* fileName, std::unordered_map<st
                 {
                     if (c > 0x7e)
                     {
+                        _setmode(_fileno(stdout), _O_WTEXT);
                         std::wcerr << L"ERROR: the entry name " << Encoding::Utf8ToUtf16(EntryName) << "at line" << lineCount << "contains non-ASCII characters!" << "Only ASCII characters can be used for entry names.";
                         continue;
                     }
